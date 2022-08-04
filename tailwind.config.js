e79e42cee2ee59cec74f6c6e23181f500de4572a
neha-359 <nehaj.strokeinfotech@gmail.com>
2022-08-04 13:00:15 +0530

    Initial project

---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js,ts,tsx}"],
+  theme: {
+    textColor: {
+      'primary': '#3e484f',
+      'secondary': '#404a51',
+      'info':'#898e92',
+      'danger': '#898e92',
+      'button-text':"#fff",
+      
+    },
+    borderColor:{
+      'real-gray':'#d8dadb'
+    },
+    backgroundColor:{
+      "input-hover":"#fdf4db",
+      "disabled" :"#fff",
+      "button-bg" :"#3e484f",
+      "page-background":"#f7fafc",
+      "page":"#fff"
+    },
+    boxShadow:{
+      "card": "0px 0px 1px 0px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 6%), 0px 0px 2px 0px rgb(0 0 0 / 6%)"
+    },
+    fontSize: {
+      xs: "12px",
+      xl: "36px"
+    },
+    maxWidth: {
+      // '1/2': '50%',
+      'a4 ': '210mm',
+    },
+    maxHeight:{
+      'a4': '297mm'
+    },
+    top:{
+      '16px':'16px'
+    },
+    right:{
+      "-20px":"-20px"
+    },
+    
+    
+    extend: {
+      width: {
+        'a4 ': '210mm',
+        'div-width':"40%",
+        'input-width':"48%"
+      },
+      height: {
+        'a4': '297mm'
+      },
+      colors: {
+        'gray': '#3e484f',
+      },
+      
+
+    },
+    
+  },
+  plugins: [],
+}
